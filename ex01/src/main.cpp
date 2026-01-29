5d974beedfc4851529b8b17db633ae6885a7a364
tjooris <tom.jooris@hotmail.fr>
2026-01-29 19:15:54 +0100

    the end

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -8,7 +8,7 @@ int    main(int argc, char** argv)
 {
     if (argc != 2)
     {
-        std::cout << "Error: invalid number of arguments." << std::endl;
+        std::cerr << "Error: invalid number of arguments." << std::endl;
         return 1;
     }
 
@@ -19,7 +19,7 @@ int    main(int argc, char** argv)
     }
     catch (const std::exception& e)
     {
-        std::cout << e.what() << std::endl;
+        std::cerr << e.what() << std::endl;
         return 1;
     }
 
